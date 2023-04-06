815d39e1123619529d71b7b123c413f54165b19e
Davian Morris <davianmorris35@gmail.com>
2023-04-06 00:12:01 -0700

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+//Random
+const random = Math.floor(Math.random() * 100) + 1;
 
-// Write your code here
+//Mod
+const num3 = 17;
+const num4 = 13;
+let mod = num3 % num4;
+
+//Max
+let numArray = [5, 10, 15, 20];
+let max = Math.max(...numArray);
